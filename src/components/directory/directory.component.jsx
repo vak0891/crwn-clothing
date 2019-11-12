c406d99b98ec04b4c32428c039c141ff26c108ac
vakjc <vaishnavi@jumpcut.com>
2019-11-12 10:11:03 +0400

    Styling, break into components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import data from "./directory.data";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            sections: data
+        }
+    }
+
+    render(){
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
