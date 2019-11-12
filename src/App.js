5e845fde925c8cb0fc7b1097b12ac6aa6d065fad
vakjc <vaishnavi@jumpcut.com>
2019-11-12 18:12:19 +0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage}/>
+      <Route exact path="/hats" component={HatsPage}/>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -15,7 +15,7 @@ class Directory extends React.Component{
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)
+                    this.state.sections.map(({id, ...othersections}) => <MenuItem key={id} {...othersections}/>)
                 }
             </div>
         )
