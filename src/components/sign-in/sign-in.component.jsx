02877bcf2db362b4d976d0537039e43a35bc590f
vakjc <vaishnavi@jumpcut.com>
2019-11-17 23:09:14 +0400

    Sign In and sign up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component{
     constructor(){
@@ -20,22 +20,33 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.setState({
-            email: "",
-            password: ""
-        });
+
+        const {email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: "",
+                password: ""
+            });
+        }
+        catch(error){
+            console.log("Error", error.message);
+        }
+
     }
 
     render(){
+        const {email, password} = this.state;
         return(
             <div className="sign-in" onSubmit={this.handleSubmit}>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form>
-                    <FormInput name="email" value={this.state.email} required type="email" handleChange={this.handleChange} label="email"/>
-                    <FormInput name="password" value={this.state.password} required type="password" handleChange={this.handleChange} label="password"/>
+                    <FormInput type="email" name="email" value={email} required  handleChange={this.handleChange} label="Email"/>
+                    <FormInput type="password" name="password" value={password} required handleChange={this.handleChange} label="Password"/>
                     <div className="buttons">
                     <CustomButton type="submit"> Sign In </CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google </CustomButton>
