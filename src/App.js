e19704236aec7e0e681ad75233931847b20b01ee
vakjc <vaishnavi@jumpcut.com>
2019-11-17 18:18:38 +0400

    Sign Up Component with storing in DB

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 class App extends React.Component {
@@ -19,8 +19,22 @@ class App extends React.Component {
   unSubscribeFromAuth = null
 
   componentDidMount(){
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //this.setState({currentUser: user})
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      this.setState({
+        currentUser: userAuth
+      })
     })
   }
 
